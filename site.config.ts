3e9cba91a549436d81ce744778a8aa68cfe98704
Bridger Tower <bridgertower@gmail.com>
2024-12-27 13:02:07 -0700

    set up

---
+++ site.config.ts
@@ -0,0 +1,13 @@
+type Config = {
+  name: string;
+  title: string;
+  desc: string;
+  url: string;
+};
+
+export const site: Config = {
+  name: "Bridger Tower",
+  title: "Bridger Tower / Design Engineer / AI Developer",
+  desc: "Designer and software engineer exploring user interface design, applied artificial intelligence, and human computer interaction.",
+  url: "https://bridger.to",
+};
